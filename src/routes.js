d2e968abd133d78afc4a61c20b979e2c59c0f3d1
Reinaldo Almeida <reinaldo.almeida@live.com>
2021-03-17 19:47:17 -0300

    [FIX] Route with audio file

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,12 @@ const express = require("express");
 
 const IndexController = require("./controllers/IndexController");
 const AuthController = require("./controllers/AuthController");
-const SpeechToTextController = require("./model/SpeechToText");
-const Blob = require("./model/Blob");
+const SpeechToTextController = require("./controllers/SpeechToTextController");
 
 const routes = express.Router();
 
 routes.get("/", IndexController.show);
 routes.get("/auth", AuthController.show);
 routes.get("/stt", SpeechToTextController.stt);
-routes.get("/blob", Blob.stt);
 
 module.exports = routes;
